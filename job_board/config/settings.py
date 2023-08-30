7b76645f049eac839b52843f436d29a2a5fc4f61
Teri Eyenike <teyenike1@gmail.com>
2023-08-30 13:16:24 +0100

    Feat: configuration of app in settings.py

--- job_board/config/settings.py
+++ job_board/config/settings.py
@@ -76,23 +76,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-import os
-import dj_database_url
-from dotenv import load_dotenv
-
-load_dotenv()
-
-database = os.getenv("DATABASE_URL")
-
 DATABASES = {
-    "default": dj_database_url.parse(database)
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
 }
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
 
 
 # Password validation
